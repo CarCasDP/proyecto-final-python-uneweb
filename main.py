0171a0e67c60670e3a3295ad2ba81555cde3a3df
aristides1000 <aristides1000@gmail.com>
2024-09-26 20:02:31 -0400

    Feat: Add exceptions to base-salary

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from decimal import Decimal, InvalidOperation
+
+def is_decimal(s):
+    try:
+        Decimal(s)
+        return True
+    except InvalidOperation:
+        return False
+
 employees = []
 
 def verify_str(x):
@@ -5,6 +14,14 @@ def verify_str(x):
   if not(employees[x]['first-name'].isalpha()):
     print('El dato ingresado no es una cadena de texto, por favor intentelo nuevamente:')
     verify_str(x)
+
+def verify_float(x):
+  employees[x]['base-salary'] = input()
+  if not(is_decimal(employees[x]['base-salary'])):
+    print('El dato ingresado no es un numero decimal, por favor intentelo nuevamente:')
+    verify_float(x)
+  employees[x]['base-salary'] = float(employees[x]['base-salary'])
+  employees[x]['base-salary'] = "{:.2f}".format(employees[x]['base-salary'])
 
 for x in range(0, 10):
   employees.append(
@@ -23,8 +40,8 @@ for x in range(0, 10):
   print('Por favor ingrese el apellido del empleado ' + str(x + 1) + ':')
   verify_str(x)
 
-  print('Por favor ingrese el sueldo base del empleado ' + str(x + 1) + ':')
-  employees[x]['base-salary'] = input()
+  print('Por favor ingrese el sueldo base del empleado ' + str(x + 1) + ', colocandolo como un numero decimal de 2 cifras, especificando los números decimales con punto y no con coma:')
+  verify_float(x)
 
   print('Por favor ingrese la fecha de ingreso del empleado ' + str(x + 1) + ':')
   employees[x]['entry-date'] = input()
