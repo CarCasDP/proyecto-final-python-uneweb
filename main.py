dd22cf67c766c824b406e0685f8b48f10c32a454
aristides1000 <aristides1000@gmail.com>
2024-09-27 19:34:15 -0400

    Feat: Add first part of exception of entry-date

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ def verify_date(x):
   if not(format_check(employees[x]['entry-date'], '%Y-%m-%d')):
     print('El dato ingresado no posee el formato de fecha deseado "yyyy-mm-dd", por favor intentelo nuevamente:')
     verify_date(x)
-  year, month, day = map(int, employees[x]['entry-date'].split('-')) # Verificar tipo de dato de "employees[x]['entry-date']"
+  print(employees[x]['entry-date'])
+  year, month, day = map(int, employees[x]['entry-date'].split('-'))
   employees[x]['entry-date'] = datetime.date(year, month, day)
 
 for x in range(0, 10):
@@ -47,6 +48,7 @@ for x in range(0, 10):
       'number-of-children': 0
     }
   )
+
 
   print('Por favor ingrese el nombre del empleado ' + str(x + 1) + ':')
   verify_str(x)
